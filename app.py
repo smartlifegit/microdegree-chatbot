a8b4df16b9533a94f1cf79466065c7d42dbecd39
audrey <smartlife.ai@gmail.com>
2025-05-20 13:27:54 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -22,4 +22,6 @@ def hello():
     return "마이크로디그리 챗봇 서버 정상동작 중!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
